9921bf4efecdde57820086c568049e08f01db076
Melledy <121644117+Melledy@users.noreply.github.com>
2025-11-17 20:23:08 -0800

    Update data version to 48

--- src/main/java/emu/nebula/GameConstants.java
+++ src/main/java/emu/nebula/GameConstants.java
@@ -3,7 +3,7 @@ package emu.nebula;
 import java.time.ZoneId;
 
 public class GameConstants {
-    private static final int DATA_VERSION = 46;
+    private static final int DATA_VERSION = 48;
     private static final String VERSION = "1.0.0";
     
     public static final ZoneId UTC_ZONE = ZoneId.of("UTC");
